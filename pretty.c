556d0e3587214d093b5fcde13036c887c735a9ec
Mike H <matroxmike@icloud.com>
2019-05-23 12:07:37 +1000

    building the sequence

--- pretty.c
+++ pretty.c
@@ -1,34 +1,41 @@
 #include <stdio.h>
+
 #include <ctype.h>
+
 #include <stdlib.h>
+
 #include <string.h>
+
 #include <stdbool.h>		//, one can use the more intuitive name bool
+
 #include <assert.h>
+
 #include "seqTest.h"
-// to build the sequence 
+ // to build the sequence 
 
 int table[16];
 
 int
 buildSeq ()
 {
-  for (int i = 0; i < 16; i++)
+  int i;
+  for (i = 0; i < 16; i++)
     {
       //  initialise the table 
-      table[i] = 0;
+      table[i] = 3;
     }
-  for (int i = 0; i < 16; i++)
+  for (i = 0; i < 16; i++)
     {
-      if ((i % 1) == 1)
+      if ((i % 1) == 0)
 	table[i] = table[i] + 1;
-      if ((i % 2) == 1)
-	table[i] = table[i] + 10;
-      if ((i % 4) == 1)
-	table[i] = table[i] + 100;
-      if ((i % 8) == 1)
+      if ((i % 2) == 0)
+	//      table[i] = table[i] + 10;
+	if ((i % 4) == 0)
+	  table[i] = table[i] + 100;
+      if ((i % 8) == 0)
 	table[i] = table[i] + 1000;
     }
-  for (int i = 0; i < 16; i++)
+  for (i = 0; i < 16; i++)
     {
       printf ("%3d  table[%d] \n", i, table[i]);
     }
